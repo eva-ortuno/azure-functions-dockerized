ca7971c36b684f6071cbef85cbb7899d1474e7f7
Eva Ortuno <ortuno.eva.pro@gmail.com>
2024-03-12 13:25:10 +0100

    With File path

--- src/functions/upload.ts
+++ src/functions/upload.ts
@@ -5,22 +5,23 @@ import { getBlobClient } from "../lib/getBlobClient";
 
 interface UploadBody {
   version: string;
-  file: string;
+  filePath: string;
 }
 
 export async function httpUpload(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-  const { version, file } = (await request.json()) as UploadBody;
-  if (!file || !version) {
+  const { version, filePath } = (await request.json()) as UploadBody;
+
+  if (!filePath || !version) {
     return {
       status: 400,
       body: "Version and file parameters are required in the request body.",
     };
   }
 
-  const fileName = `${new Date().toDateString()}_${version}.txt`;
+  const fileName = `${new Date().toDateString()}_${version}.pdf`;
 
   const blockBlobClient = getBlobClient(fileName);
-  await blockBlobClient.uploadData(Buffer.from(file, "utf-8"));
+  await blockBlobClient.uploadFile(filePath);
 
   return {
     status: 200,
